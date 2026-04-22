27e81cb6bb98b9ab35ac554097a9d7062fd9de7f
Cristian Rojas <credrope@hotmail.com>
2026-04-22 11:45:40 -0500

    add retry policy to structure dag

--- dags/meetup_structure_snowflake.py
+++ dags/meetup_structure_snowflake.py
@@ -1,13 +1,22 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+
+DEFAULT_RETRY_ARGS = {
+    "owner": "cristian",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2),
+    "retry_exponential_backoff": True,
+    "max_retry_delay": timedelta(minutes=5),
+}
 
 with DAG(
     dag_id="meetup_structure",
     start_date=datetime(2026, 4, 1),
     schedule=None,
     catchup=False,
+    default_args=DEFAULT_RETRY_ARGS,
     tags=["meetup", "structure"],
 ) as dag:
 
